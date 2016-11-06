190861c4980766efb4af05b0246bd52d36c943d9
pJenniferN <pnaiman1@jhu.edu>
2016-11-06 14:42:59 -0500

    Assignment 6/1

---
+++ mod_6/app.js
@@ -0,0 +1,38 @@
+(function () {
+	'use strict'	
+	
+	angular.module("LunchCheck", [])
+	.controller("LunchCheckController", LunchCheckController);
+	LunchCheckController.$inject = ['$scope'];
+	function LunchCheckController($scope) {
+		$scope.list = "";
+		$scope.message = "";
+		$scope.borderColor = "";
+		$scope.messageColor = "";
+		$scope.calcEat = function() {
+			if ($scope.list === "") {
+				$scope.message = "Please enter data first";
+				$scope.borderColor = "borderRed";
+				$scope.messageColor = "red";
+			}
+			else if (numItemsInString($scope.list) > 3) {
+				$scope.message = "To Much!";
+				$scope.borderColor = "borderGreen";
+				$scope.messageColor = "green";
+			} else {
+				$scope.message = "Enjoy!";
+				$scope.borderColor = "borderGreen";
+				$scope.messageColor = "green";
+			}
+		}
+	}
+	
+	function numItemsInString(myString) {
+		var stringArray = myString.split(',');
+		var filteredArray = stringArray.filter(function(item) {
+			return item != "";
+		});
+		return filteredArray.length;
+	}
+	
+})()
