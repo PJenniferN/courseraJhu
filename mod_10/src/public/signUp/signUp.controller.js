7416079ccd13f3ec519850da08f17c1baa143cf7
pJenniferN <pnaiman1@jhu.edu>
2016-12-12 22:54:08 -0500

    Final Assignment

---
+++ mod_10/src/public/signUp/signUp.controller.js
@@ -0,0 +1,13 @@
+(function () {
+angular.module('public')
+.controller('SignUpController', SignUpController);
+
+function SignUpController() {
+  var sign = this;
+
+  sign.submit = function () {
+    sign.completed = true;
+  };
+}
+
+})();
