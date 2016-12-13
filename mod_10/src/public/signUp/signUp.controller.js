0934552d4bb75d944eb53c21b929bfc4b91dfc68
pJenniferN <pnaiman1@jhu.edu>
2016-12-12 23:00:01 -0500

    fixed indentation

--- mod_10/src/public/signUp/signUp.controller.js
+++ mod_10/src/public/signUp/signUp.controller.js
@@ -1,13 +1,14 @@
 (function () {
-angular.module('public')
-.controller('SignUpController', SignUpController);
-
-function SignUpController() {
-  var sign = this;
-
-  sign.submit = function () {
-    sign.completed = true;
-  };
-}
+	
+	angular.module('public')
+	.controller('SignUpController', SignUpController);
+	
+	function SignUpController() {
+	  var sign = this;
+	
+	  sign.submit = function () {
+	    sign.completed = true;
+	  };
+	}
 
 })();
